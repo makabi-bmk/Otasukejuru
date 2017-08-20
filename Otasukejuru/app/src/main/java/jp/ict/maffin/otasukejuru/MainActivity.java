7a2e5fe9f97a0b0430e85c6c1568a041f7b3f329
mitohato <mitohato14@gmail.com>
2017-08-20 18:10:52 +0900

    Rename package

---
+++ Otasukejuru/app/src/main/java/jp/ict/maffin/otasukejuru/MainActivity.java
@@ -0,0 +1,13 @@
+package jp.ict.maffin.otasukejuru;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(jp.ict.maffin.otasukejuru.R.layout.activity_main);
+    }
+}
