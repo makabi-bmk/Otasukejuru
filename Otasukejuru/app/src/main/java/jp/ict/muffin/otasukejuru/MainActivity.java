0053734246f300297817b7b044775c03d345df6d
mitohato <mitohato14@gmail.com>
2017-08-22 09:05:04 +0900

    Rename package

--- Otasukejuru/app/src/main/java/jp/ict/muffin/otasukejuru/MainActivity.java
+++ Otasukejuru/app/src/main/java/jp/ict/muffin/otasukejuru/MainActivity.java
@@ -1,4 +1,4 @@
-package jp.ict.maffin.otasukejuru;
+package jp.ict.muffin.otasukejuru;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -8,6 +8,6 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(jp.ict.maffin.otasukejuru.R.layout.activity_main);
+        setContentView(jp.ict.muffin.otasukejuru.R.layout.activity_main);
     }
 }
