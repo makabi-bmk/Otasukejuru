821b67efc6134a4e91e51e6a716fb60271bdfa48
mitohato <mitohato14@gmail.com>
2017-08-24 22:29:40 +0900

    Divide ui and fragment in TimerFragment and Call ui from Fragment

--- Otasukejuru/app/src/main/kotlin/jp/ict/muffin/otasukejuru/TimerFragment.kt
+++ Otasukejuru/app/src/main/kotlin/jp/ict/muffin/otasukejuru/TimerFragment.kt
@@ -2,58 +2,23 @@ package jp.ict.muffin.otasukejuru
 
 import android.os.Bundle
 import android.support.v4.app.Fragment
-import android.view.Gravity
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import org.jetbrains.anko.*
-import org.jetbrains.anko.support.v4.UI
+import org.jetbrains.anko.AnkoContext
+import org.jetbrains.anko.support.v4.ctx
 
 
 /**
  * Created by mito on 2017/08/23.
  */
 class TimerFragment : Fragment() {
-    lateinit var countDown: CountDown
-    var isPushStartButton = false
     companion object {
         fun getInstance(): TimerFragment = TimerFragment()
     }
     
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View =
-            UI {
-                verticalLayout {
-                    lparams {
-                        gravity = Gravity.CENTER_HORIZONTAL
-                    }
-                    val timerText = textView {
-                        text = getString(R.string.initTime)
-                        textSize = 20f
-                        gravity = Gravity.CENTER_HORIZONTAL
-                        topPadding = dip(150)
-                    }
-                    button("start") {
-                        height = wrapContent
-                        width = 50
-                        gravity = Gravity.CENTER_HORIZONTAL
-                    }.setOnClickListener {
-                        if (!isPushStartButton) {
-                            countDown = CountDown(180000, 100, timerText)
-                            countDown.start()
-                            isPushStartButton = true
-                        }
-                    }
-                    button("stop") {
-                    }.setOnClickListener {
-                        if (isPushStartButton) {
-                            countDown.cancel()
-                            timerText.text = getString(R.string.initTime)
-                            isPushStartButton = false
-                        }
-                        
-                    }
-                }
-            }.view
+    override fun onCreateView(inflater: LayoutInflater?, container: ViewGroup?, savedInstanceState: Bundle?): View? =
+            TimerFragmentUI().createView(AnkoContext.create(ctx, this))
     
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
---
+++ Otasukejuru/app/src/main/kotlin/jp/ict/muffin/otasukejuru/TimerFragmentUI.kt
@@ -0,0 +1,47 @@
+package jp.ict.muffin.otasukejuru
+
+import android.view.Gravity
+import android.view.View
+import org.jetbrains.anko.*
+
+/**
+ * Created by mito on 2017/08/24.
+ */
+class TimerFragmentUI : AnkoComponent<TimerFragment>{
+    lateinit var countDown: CountDown
+    var isPushStartButton = false
+    
+    override fun createView(ui: AnkoContext<TimerFragment>): View = with(ui) {
+        
+        verticalLayout {
+            lparams {
+                gravity = Gravity.CENTER_HORIZONTAL
+            }
+            val timerText = textView {
+                text = context.getString(R.string.initTime)
+                textSize = 20f
+                gravity = Gravity.CENTER_HORIZONTAL
+                topPadding = dip(150)
+            }
+            button("start") {
+                height = wrapContent
+                width = 50
+                gravity = Gravity.CENTER_HORIZONTAL
+            }.setOnClickListener {
+                if (!isPushStartButton) {
+                    countDown = CountDown(180000, 100, timerText)
+                    countDown.start()
+                    isPushStartButton = true
+                }
+            }
+            button("cancel") {
+            }.setOnClickListener {
+                if (isPushStartButton) {
+                    countDown.cancel()
+                    timerText.text = context.getString(R.string.initTime)
+                    isPushStartButton = false
+                }
+            }
+        }
+    }
+}
