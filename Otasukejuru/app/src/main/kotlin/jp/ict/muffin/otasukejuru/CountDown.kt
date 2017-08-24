552a58b909fa604e59cb5835bbfa6f7230da1dda
mitohato <mitohato14@gmail.com>
2017-08-24 22:46:58 +0900

    Clean code

--- Otasukejuru/app/src/main/kotlin/jp/ict/muffin/otasukejuru/CountDown.kt
+++ Otasukejuru/app/src/main/kotlin/jp/ict/muffin/otasukejuru/CountDown.kt
@@ -1,20 +1,16 @@
 package jp.ict.muffin.otasukejuru
 
+import android.app.Activity
 import android.os.CountDownTimer
 import android.widget.TextView
 
 /**
  * Created by mito on 2017/08/24.
  */
-class CountDown: CountDownTimer {
-    private val timerText: TextView
-    
-    constructor(millisUntilFinished: Long, countDownInterval: Long, timerText: TextView) : super(millisUntilFinished, countDownInterval) {
-        this.timerText = timerText
-    }
+class CountDown(millisUntilFinished: Long, countDownInterval: Long, private val timerText: TextView) : CountDownTimer(millisUntilFinished, countDownInterval) {
     
     override fun onFinish() {
-        timerText.text = "0:00.000"
+        timerText.text = Activity().getString(R.string.initTime)
     }
     
     override fun onTick(millisUntilFinished: Long) {
