59e901078a8b398dfaf81164635c438c68ea2785
mitohato <mitohato14@gmail.com>
2017-08-24 13:12:02 +0900

    Create layout in Anko

--- Otasukejuru/app/src/main/kotlin/jp/ict/muffin/otasukejuru/TimerFragment.kt
+++ Otasukejuru/app/src/main/kotlin/jp/ict/muffin/otasukejuru/TimerFragment.kt
@@ -2,23 +2,48 @@ package jp.ict.muffin.otasukejuru
 
 import android.os.Bundle
 import android.support.v4.app.Fragment
+import android.view.Gravity
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import org.jetbrains.anko.*
+import org.jetbrains.anko.support.v4.UI
 
 /**
  * Created by mito on 2017/08/23.
  */
-public class TimerFragment : Fragment() {
+class TimerFragment : Fragment() {
     companion object {
         fun getInstance(): TimerFragment = TimerFragment()
     }
     
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View =
-            inflater.inflate(R.layout.fragment_timer, container, false)
+            UI {
+                verticalLayout {
+                    lparams {
+                        gravity = Gravity.CENTER_HORIZONTAL
+                    }
+                    textView {
+                        text = "00:00.0"
+                        textSize = 20f
+                        gravity = Gravity.CENTER_HORIZONTAL
+                        topPadding = dip(150)
+                    }
+                    button("start") {
+                        height = wrapContent
+                        width = 50
+                        gravity = Gravity.CENTER_HORIZONTAL
+                    }
+                    button("stop") {
+                        
+                    }
+                }
+            }.view
     
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
         
     }
+    
+    
 }
