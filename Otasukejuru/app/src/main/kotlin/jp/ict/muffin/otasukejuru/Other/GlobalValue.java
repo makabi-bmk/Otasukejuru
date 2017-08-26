aeb56089cfabd2ec938d3f241c93a6fa491f8517
mitohato <mitohato14@gmail.com>
2017-08-26 16:51:41 +0900

    Add GlobalValue

---
+++ Otasukejuru/app/src/main/kotlin/jp/ict/muffin/otasukejuru/Other/GlobalValue.java
@@ -0,0 +1,24 @@
+package jp.ict.muffin.otasukejuru.Other;
+
+/**
+ * Created by mito on 2017/08/26.
+ */
+
+public class GlobalValue {
+    private static int displayWidth;
+    private static int displayHeight;
+
+    public int getDisplayWidth() {
+        return displayWidth;
+    }
+    public int getDisplayHeight() {
+        return displayHeight;
+    }
+
+    public void setDisplayWidth(int width) {
+        displayWidth = width;
+    }
+    public void setDisplayHeight(int height) {
+        displayHeight = height;
+    }
+}
