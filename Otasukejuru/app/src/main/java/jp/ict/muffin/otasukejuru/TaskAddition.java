8d770a7f90b0b42f1f505835566f608d3ef0d30b
b-mk <youremailexample.com(mi161332@edu.okinawa-ct.ac.jp)>
2017-08-27 19:36:47 +0900

    add TaskAddition

--- Otasukejuru/app/src/main/java/jp/ict/muffin/otasukejuru/TaskAddition.java
+++ Otasukejuru/app/src/main/java/jp/ict/muffin/otasukejuru/TaskAddition.java
@@ -1,42 +1,206 @@
 package jp.ict.muffin.otasukejuru;
 
+import android.app.Activity;
 import android.os.Bundle;
-import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentActivity;
-import android.support.v4.app.FragmentTransaction;
 import android.view.View;
-import android.widget.TextView;
-import android.widget.Toast;
+import android.widget.Button;
 
-public class TaskAddition extends Fragment {
+public class TaskAddition extends Activity {
 
-    //@Override
-    public View onCreateView(Bundle savedInstanceState) {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.selection);
 
-        View fragment = new View(getActivity());
-
-        TextView message = (TextView) fragment.findViewById(R.id.message);
-        message.setText("何を追加しますか？");
-
-        // はい ボタンのリスナ
-        fragment.findViewById(R.id.plan).setOnClickListener(new View.OnClickListener() {
+        Button plan = (Button) findViewById(R.id.plan);
+        plan.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-            }
-
-        });
-
-        // いいえ ボタンのリスナ
-        fragment.findViewById(R.id.task).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-
+                plan1();
             }
         });
 
-        int a = 0;
-
-        return fragment;
+        Button task = (Button) findViewById(R.id.task);
+        task.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                task1();
+            }
+        });
     }
 
+    private void plan1(){
+        setContentView(R.layout.plan1);
+        Button next = (Button) findViewById(R.id.next);
+        next.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                plan2();
+            }
+        });
+
+    }
+
+    private void plan2(){
+        setContentView(R.layout.plan2);
+        Button next = (Button) findViewById(R.id.next);
+        next.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                plan3();
+            }
+        });
+
+    }
+    private void plan3(){
+        setContentView(R.layout.plan3);
+        Button next = (Button) findViewById(R.id.next);
+        next.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                plan4();
+            }
+        });
+
+    }
+    private void plan4(){
+        setContentView(R.layout.plan4);
+        Button next = (Button) findViewById(R.id.next);
+        next.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                plan5();
+            }
+        });
+
+    }
+
+    private void plan5(){
+        setContentView(R.layout.plan5);
+        Button next = (Button) findViewById(R.id.next);
+        next.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+
+    }
+
+
+    private void task1(){
+        setContentView(R.layout.task1);
+        Button next = (Button) findViewById(R.id.next);
+        next.setOnClickListener(
+                new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                task2();
+            }
+        });
+
+    }
+
+    private void task2(){
+        setContentView(R.layout.task2);
+        Button next = (Button) findViewById(R.id.next);
+        next.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                plan3();
+            }
+        });
+
+        Button noPeriod = (Button) findViewById(R.id.noPeriod);
+        next.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                plan3();
+            }
+        });
+
+    }
+
+    private void task3(){
+        setContentView(R.layout.task3);
+        Button next = (Button) findViewById(R.id.next);
+        next.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                task4();
+            }
+        });
+
+    }
+
+    private void task4(){
+        setContentView(R.layout.task4);
+        Button no = (Button) findViewById(R.id.no);
+        no.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                task5();
+            }
+        });
+
+        Button yes = (Button) findViewById(R.id.yes);
+        yes.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                task5();
+            }
+        });
+    }
+
+    private void task5(){
+        setContentView(R.layout.task5);
+        Button no = (Button) findViewById(R.id.no);
+        no.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                task6();
+            }
+        });
+
+        Button yes = (Button) findViewById(R.id.yes);
+        yes.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                task6();
+            }
+        });
+
+    }
+
+    private void task6(){
+        setContentView(R.layout.task6);
+        Button no = (Button) findViewById(R.id.no);
+        no.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                task7();
+            }
+        });
+
+        Button yes = (Button) findViewById(R.id.yes);
+        yes.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                task7();
+            }
+        });
+
+    }
+
+    private void task7(){
+        setContentView(R.layout.task7);
+        Button next = (Button) findViewById(R.id.next);
+        next.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+
+    }
 }
