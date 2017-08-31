7f744bbf7347126baeea5e8f58a64aa31264de6a
mitohato <mitohato14@gmail.com>
2017-09-01 04:46:48 +0900

    Draw circle by to input time

--- Otasukejuru/app/src/main/kotlin/jp/ict/muffin/otasukejuru/Fragment/TimerFragment.kt
+++ Otasukejuru/app/src/main/kotlin/jp/ict/muffin/otasukejuru/Fragment/TimerFragment.kt
@@ -1,17 +1,12 @@
 package jp.ict.muffin.otasukejuru
 
-import android.graphics.Color
 import android.os.Bundle
 import android.support.v4.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.FrameLayout
-import jp.ict.muffin.otasukejuru.View.CircleGraphView
 import org.jetbrains.anko.AnkoContext
-import org.jetbrains.anko.backgroundColor
 import org.jetbrains.anko.support.v4.ctx
-import org.jetbrains.anko.support.v4.find
 
 
 /**
@@ -28,16 +23,5 @@ class TimerFragment : Fragment() {
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
         
-        val circle = find<FrameLayout>(1)
-        circle.backgroundColor = Color.argb(0, 0, 0, 0)
-        
-        val time = 25L
-        val circleGraphView = CircleGraphView(context, Color.argb(255, 255, 0, 0), time, true)
-        circle.addView(circleGraphView)
-        circleGraphView.startAnimation()
-
-        val circleGraphView1 = CircleGraphView(context, Color.argb(255, 255, 255, 255), time, false)
-        circle.addView(circleGraphView1)
-        circleGraphView1.startAnimation()
     }
 }
