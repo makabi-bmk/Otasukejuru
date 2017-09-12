89a298880cb3f52f877f0bf5163a745bb851d5d3
nemusou <ryubi.102395@gmail.com>
2017-09-13 00:18:27 +0900

    defined class Task and Schedule

---
+++ serverapp/defineclass.py
@@ -0,0 +1,45 @@
+import random
+import datetime as dt
+
+
+class Task:
+    def __init__(self, task_name, regi_date, due_date, priority, run_time):
+        self.Registration_date: dt.datetime = regi_date  # 登録日
+        self.due_date: dt.datetime = due_date  # 期限
+        self.task_name: str = task_name
+        self.priority: int = priority
+        self.run_time: int = run_time  # 作業時間(単位(時間))
+        self.progress: float = 0.
+
+    def __lt__(self, other):
+        if self.priority == other.priority:
+            if self.progress == other.progress:
+                return self.due_date < other.due_date
+            else:
+                return self.progress < other.progress
+        else:
+            return self.priority > other.priority
+
+    def update_priority(self, new_priority) -> None:
+        # 優先度が決まるのって期限までの日数でだっけ
+        self.priority = new_priority
+
+    def update_progress(self, new_progress) -> None:
+        self.progress = new_progress
+
+
+class Schedule:
+    def __init__(self, schedule_name, date):
+        self.schedule_name: str = schedule_name
+        self.date = date
+
+
+if __name__ == '__main__':
+    l = [Task(str(i), dt.datetime.now(), dt.datetime.now(), i % 2, i * 10) for i
+         in range(10)]
+    random.shuffle(l)
+    s = sorted(l)
+    for i in s:
+        print(
+            "x: {}, due_date: {}, priority: {}".format(i.task_name, i.due_date,
+                                                       i.priority))
