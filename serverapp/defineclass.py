cd55ae7c468c7b2e34a218011c48c547aaa70363
nemusou <ryubi.102395@gmail.com>
2017-09-18 16:13:07 +0900

    add Every class change smart sort

--- serverapp/defineclass.py
+++ serverapp/defineclass.py
@@ -13,15 +13,6 @@ class Task:
         self.run_time: int = run_time  # 作業時間(単位(時間))
         self.progress: float = 0.
 
-    def __lt__(self, other):
-        if self.priority == other.priority:
-            if self.progress == other.progress:
-                return self.due_date < other.due_date
-            else:
-                return self.progress < other.progress
-        else:
-            return self.priority > other.priority
-
     def update_priority(self, new_priority: int) -> None:
         # 優先度が決まるのって期限までの日数でだっけ
         self.priority = new_priority
@@ -31,16 +22,24 @@ class Task:
 
 
 class Schedule:
-    def __init__(self, schedule_name: int, date: dt.datetime):
+    def __init__(self, schedule_name: str, date: dt.datetime):
         self.schedule_name: str = schedule_name
         self.date = date
+
+
+class Every:
+    def __init__(self, name: str, start_time: dt.datetime,
+                 end_time: dt.datetime):
+        self.name: str = name
+        self.start_time: dt.datetime = start_time
+        self.end_time: dt.datetime = end_time
 
 
 if __name__ == '__main__':
     l = [Task(str(i), dt.datetime.now(), dt.datetime.now(), i % 2, i * 10) for i
          in range(10)]
     random.shuffle(l)
-    s = sorted(l)
+    s = sorted(l, key=lambda x: (-x.priority, x.progress, x.due_date))
     for i in s:
         print(
             "x: {}, due_date: {}, priority: {}".format(i.task_name, i.due_date,
