611aa5596b3b8b7215c5c36b0ead44383a22b725
nemusou <ryubi.102395@gmail.com>
2017-09-22 18:19:20 +0900

    change the data to put in DB

--- serverapp/dbcon.py
+++ serverapp/dbcon.py
@@ -1,7 +1,8 @@
 import datetime as dt
 from logging import getLogger, StreamHandler, DEBUG
 from pymongo import MongoClient
-from serverapp.defineclass import Task, Schedule, Every
+
+# from serverapp.defineclass import Task, Schedule, Every
 
 logger = getLogger(__name__)
 handler = StreamHandler()
@@ -18,34 +19,34 @@ task_col = client['task']
 
 def add_task(task_name: str, registration_date: dt.datetime,
              due_date: dt.datetime, priority: int):
-    task = Task(task_name, registration_date, due_date, priority, run_time=0)
-    post = {"task": task}
-    # post = {
-    #    "task_name": task_name,
-    #    "registration_date": registration_date,
-    #    "due_date": due_date,
-    #    "priority": priority,
-    #    "run_time": run_time
-    # }
+    # task = Task(task_name, registration_date, due_date, priority, run_time=0)
+    # post = {"task": task}
+    post = {
+        "task_name": task_name,
+        "registration_date": registration_date,
+        "due_date": due_date,
+        "priority": priority,
+        "run_time": 0
+    }
     task_col.insert_one(post)
 
 
 def add_schedule(schedule_name: str, date: dt.datetime):
-    schedule = Schedule(schedule_name, date)
-    post = {"schedule": schedule}
-    # post = {
-    #    "schedule_name": schedule_name,
-    #    "date": date
-    # }
+    # schedule = Schedule(schedule_name, date)
+    # post = {"schedule": schedule}
+    post = {
+        "schedule_name": schedule_name,
+        "date": date
+    }
     task_col.insert_one(post)
 
 
 def add_every(name: str, start_time: dt.datetime, end_time: dt.datetime):
-    every = Every(name, start_time, end_time)
-    post = {"every": every}
-    # post = {
-    #     "name": name,
-    #     "start_time": start_time,
-    #     "end_time": end_time,
-    # }
+    # every = Every(name, start_time, end_time)
+    # post = {"every": every}
+    post = {
+        "name": name,
+        "start_time": start_time,
+        "end_time": end_time,
+    }
     every_col.insert_one(post)
