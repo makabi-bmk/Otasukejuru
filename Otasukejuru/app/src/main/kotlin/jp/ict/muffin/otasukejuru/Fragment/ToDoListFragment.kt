aa3f4ab4c968e5388d0484e8a21eef999a24b5e4
mitohato <mitohato14@gmail.com>
2017-09-30 13:36:37 +0900

    Set information of task to card view

--- Otasukejuru/app/src/main/kotlin/jp/ict/muffin/otasukejuru/Fragment/ToDoListFragment.kt
+++ Otasukejuru/app/src/main/kotlin/jp/ict/muffin/otasukejuru/Fragment/ToDoListFragment.kt
@@ -9,6 +9,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.LinearLayout
+import jp.ict.muffin.otasukejuru.MainActivity.taskInformationArrayList
 import kotlinx.android.synthetic.main.fragment_list_todo.*
 import kotlinx.android.synthetic.main.task_card_view.view.*
 import org.jetbrains.anko.support.v4.toast
@@ -23,33 +24,31 @@ class ToDoListFragment : Fragment() {
     @RequiresApi(Build.VERSION_CODES.M)
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
-        (0..3).forEach { i ->
-            (0..6).forEach { it ->
-                val inflater: LayoutInflater = context.getSystemService(LAYOUT_INFLATER_SERVICE) as LayoutInflater
-                val linearLayout: LinearLayout = inflater.inflate(R.layout.task_card_view, null) as LinearLayout
-                linearLayout.apply {
-                    dateTextView.apply {
-                        text = "1"
-                        textColor = context.resources.getColor(R.color.mostPriority)
-                    }
-                    cardView.apply {
-                        tag = it
-                        setOnClickListener {
-                            toast(it.tag.toString())
-                        }
+        (0 until taskInformationArrayList.size).forEach {
+            
+            val inflater: LayoutInflater = context.getSystemService(LAYOUT_INFLATER_SERVICE) as LayoutInflater
+            val linearLayout: LinearLayout = inflater.inflate(R.layout.task_card_view, null) as LinearLayout
+            linearLayout.apply {
+                dateTextView.apply {
+                    text = taskInformationArrayList[it].name
+                    textColor = context.resources.getColor(R.color.mostPriority)
+                }
+                cardView.apply {
+                    tag = taskInformationArrayList[it].name
+                    setOnClickListener {
+                        toast(tag.toString())
                     }
                 }
-                when (it) {
-                    0 -> mostPriorityCardLinear
-                    1 -> highPriorityCardLinear1
-                    2 -> highPriorityCardLinear2
-                    3 -> middlePriorityCardLinear1
-                    4 -> middlePriorityCardLinear2
-                    5 -> lowPriorityCardLinear1
-                    else -> lowPriorityCardLinear2
-                }.addView(linearLayout, i)
-//                mostPriorityCardLinear.addView(linearLayout, it)
             }
+            when (it / 4) {
+                0 -> mostPriorityCardLinear
+                1 -> highPriorityCardLinear1
+                2 -> highPriorityCardLinear2
+                3 -> middlePriorityCardLinear1
+                4 -> middlePriorityCardLinear2
+                5 -> lowPriorityCardLinear1
+                else -> lowPriorityCardLinear2
+            }.addView(linearLayout, it % 4)
         }
     }
 }
