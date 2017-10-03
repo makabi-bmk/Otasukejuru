13577843817e65486cb7f33b5db279b23c518833
nemusou <ryubi.102395@gmail.com>
2017-10-03 18:46:50 +0900

    add str_to_(date|time|datetime) function

---
+++ serverapp/strtime.py
@@ -0,0 +1,25 @@
+import datetime
+
+
+def str_to_time(time: str):
+    """
+    :param time: "%H:%m:%d
+    :return: datetime.datetime
+    """
+    return datetime.datetime.strptime(time, "%H:%M:%S")
+
+
+def str_to_date(date: str):
+    """
+    :param date: "%Y-%m-%d"
+    :return: datetime.datetime
+    """
+    return datetime.datetime.strptime(date, "%Y-%m-%d")
+
+
+def str_to_datetime(dtime: str):
+    """
+    :param datetime: "%Y-%m-%d %H:%M:%S"
+    :return:
+    """
+    return datetime.datetime.strptime(dtime, "%Y-%m-%d %H:%M:%S")
