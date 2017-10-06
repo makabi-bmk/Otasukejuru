68708bcf7746efcc4882419795c5488b7bb9f4f5
mitohato <mitohato14@gmail.com>
2017-10-06 10:27:07 +0900

    Add TimerSetActivity

---
+++ Otasukejuru/app/src/main/kotlin/jp/ict/muffin/otasukejuru/Activity/TimerSetActivity.kt
@@ -0,0 +1,12 @@
+package jp.ict.muffin.otasukejuru
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+class TimerSetActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_timer_set)
+    }
+}
