e64bd6e09e3928c09ca77104f10cfa4ec57d52f4
mitohato <mitohato14@gmail.com>
2017-10-11 13:59:33 +0900

    Set button and text in notificationActivity

--- Otasukejuru/app/src/main/kotlin/jp/ict/muffin/otasukejuru/UI/TimerNotificationActivityUI.kt
+++ Otasukejuru/app/src/main/kotlin/jp/ict/muffin/otasukejuru/UI/TimerNotificationActivityUI.kt
@@ -11,8 +11,57 @@ class TimerNotificationActivityUI : AnkoComponent<TimerNotificationActivity> {
         
         relativeLayout {
             backgroundColor = Color.argb(255, 251, 251, 240)
+            textView {
+                id = R.id.titleInterval
+                text = "終了前の通知"
+                textColor = Color.argb(255, 102, 183, 236)
+                textSize = 30f
+            }.lparams {
+                topMargin = dip(10)
+                alignParentStart()
+                alignParentTop()
+            }
+            
+            textView {
+                id = R.id.titleNotification
+                text = "終了何分前に\n通知しますか？"
+                textSize = 30f
+            }.lparams {
+                centerHorizontally()
+                topMargin = dip(50)
+            }
+            
+            relativeLayout {
+                numberPicker {
+                    id = R.id.notificationNumPick
+                    maxValue = 30
+                    minValue = 5
+                }.lparams {
+                    centerVertically()
+                    alignParentStart()
+                }
+                textView {
+                    id = R.id.notificationText
+                    text = "分前"
+                    textSize = 30f
+                }.lparams {
+                    centerVertically()
+                    rightOf(R.id.notificationNumPick)
+                }
+            }.lparams {
+                centerHorizontally()
+                centerVertically()
+            }
             button("次へ") {
-                
+                id = R.id.nextButton
+                backgroundColor = Color.argb(0, 0, 0, 0)
+                textColor = Color.argb(255, 102, 183, 236)
+                textSize = 20f
+            }.lparams {
+                marginEnd = dip(30)
+                bottomMargin = dip(20)
+                alignParentBottom()
+                alignParentRight()
             }.setOnClickListener {
                 startActivity<TimerActivity>()
             }
