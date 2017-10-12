29ab7f340aa91006ba0fb3c77d82752ed23befb8
mitohato <mitohato14@gmail.com>
2017-10-12 14:40:08 +0900

    Clean up

--- Otasukejuru/app/src/main/kotlin/jp/ict/muffin/otasukejuru/UI/TimerSetTimeFragmentUI.kt
+++ Otasukejuru/app/src/main/kotlin/jp/ict/muffin/otasukejuru/UI/TimerSetTimeFragmentUI.kt
@@ -29,29 +29,33 @@ class TimerSetTimeFragmentUI : AnkoComponent<TimerSetTimeFragment> {
             circle = frameLayout {
                 backgroundColor = Color.argb(0, 0, 0, 0)
                 id = R.id.circleFrame
-            }.lparams {
-                height = GlobalValue.displayWidth - 30
-                width = GlobalValue.displayWidth - 30
-                topMargin = 30
-                leftMargin = 30
+                lparams {
+                    height = GlobalValue.displayWidth - 30
+                    width = GlobalValue.displayWidth - 30
+                    topMargin = 30
+                    leftMargin = 30
+                }
+                
+            }
+            circleMini = frameLayout {
+                lparams {
+                    translationZ = 2F
+                    height = GlobalValue.displayWidth / 3
+                    width = GlobalValue.displayWidth / 3
+                    topMargin = GlobalValue.displayWidth / 3 + 15
+                    leftMargin = GlobalValue.displayWidth / 3 + 15
+                    
+                }
             }
             
-            circleMini = frameLayout {
+            remainingHourText = textView("") {
+                textSize = 40F
             }.lparams {
                 translationZ = 2F
-                height = GlobalValue.displayWidth / 3
-                width = GlobalValue.displayWidth / 3
-                topMargin = GlobalValue.displayWidth / 3 + 15
-                leftMargin = GlobalValue.displayWidth / 3 + 15
-                remainingHourText = textView("") {
-                    textSize = 40F
-                }.lparams {
-                    translationZ = 2F
-                    width = wrapContent
-                    height = wrapContent
-                    topMargin = GlobalValue.displayWidth / 2 - 40
-                    centerHorizontally()
-                }
+                width = wrapContent
+                height = wrapContent
+                topMargin = GlobalValue.displayWidth / 2 - 40
+                centerHorizontally()
             }
             
             linearLayout {
