1d1457e92ff16dacc26ca4668c9b7458e5f385e2
mitohato <mitohato14@gmail.com>
2017-10-19 11:49:11 +0900

    Add DateActivity

---
+++ Otasukejuru/app/src/main/kotlin/jp/ict/muffin/otasukejuru/activity/DateActivity.kt
@@ -0,0 +1,13 @@
+package jp.ict.muffin.otasukejuru.activity
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import jp.ict.muffin.otasukejuru.R
+
+class DateActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_date)
+    }
+}
