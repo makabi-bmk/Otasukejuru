05e5f0c58e0d62a79cd87a86a7cd9697756031fa
mitohato <mitohato14@gmail.com>
2017-10-20 08:27:26 +0900

    Refactor code in MainActivity for Kotlin

--- Otasukejuru/app/src/main/kotlin/jp/ict/muffin/otasukejuru/activity/MainActivity.kt
+++ Otasukejuru/app/src/main/kotlin/jp/ict/muffin/otasukejuru/activity/MainActivity.kt
@@ -1,40 +1,32 @@
 package jp.ict.muffin.otasukejuru.activity
 
-import android.content.Intent
 import android.graphics.Point
 import android.os.Bundle
 import android.support.design.widget.FloatingActionButton
-import android.support.design.widget.TabLayout
 import android.support.v4.app.Fragment
 import android.support.v4.app.FragmentManager
 import android.support.v4.app.FragmentPagerAdapter
 import android.support.v7.app.AppCompatActivity
-import android.support.v7.widget.Toolbar
-import android.view.LayoutInflater
-import android.view.Menu
-import android.view.MenuItem
-import android.view.View
-import android.view.ViewGroup
+import android.view.*
 import android.widget.TextView
-import jp.ict.muffin.otasukejuru.view.HoldableViewPager
 import jp.ict.muffin.otasukejuru.R
-
-import java.util.Calendar
-
-import jp.ict.muffin.otasukejuru.fragment.TimerSetTimeFragment
-import jp.ict.muffin.otasukejuru.fragment.ToDoListFragment
 import jp.ict.muffin.otasukejuru.`object`.GlobalValue
 import jp.ict.muffin.otasukejuru.fragment.CalendarFragment
+import jp.ict.muffin.otasukejuru.fragment.TimerSetTimeFragment
+import jp.ict.muffin.otasukejuru.fragment.ToDoListFragment
+import jp.ict.muffin.otasukejuru.view.HoldableViewPager
+import kotlinx.android.synthetic.main.activity_main.*
+import org.jetbrains.anko.startActivity
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
     private val globalValue = GlobalValue
-
+    
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-
-        val toolbar = findViewById(R.id.toolbar) as Toolbar
+        
+        
         val calendar = Calendar.getInstance()
         toolbar.title = (calendar.get(Calendar.MONTH) + 1).toString() + "月" +
                 calendar.get(Calendar.DATE) + "日"
@@ -50,58 +42,56 @@ class MainActivity : AppCompatActivity() {
       {@link android.support.v4.app.FragmentStatePagerAdapter}.
      */
         val mSectionsPagerAdapter = SectionsPagerAdapter(supportFragmentManager)
-
+        
         // Set up the ViewPager with the sections adapter.
         /*
       The {@link ViewPager} that will host the section contents.
      */
-
-        val mViewPager = findViewById(R.id.container) as HoldableViewPager
-        mViewPager.adapter = mSectionsPagerAdapter
-        mViewPager.setSwipeHoldVar(true)
-
-        val tabLayout = findViewById(R.id.tabs) as TabLayout
-        tabLayout.setupWithViewPager(mViewPager)
-
+        
+        val mViewPager = findViewById(R.id.view_pager) as HoldableViewPager
+        view_pager.adapter = mSectionsPagerAdapter
+        view_pager.setSwipeHoldVar(true)
+        
+        tabs.setupWithViewPager(mViewPager)
+        
         val fab = findViewById(R.id.fab) as FloatingActionButton
         fab.setOnClickListener {
-            val intent = Intent(this@MainActivity, TaskAdditionActivity::class.java)
-            startActivity(intent)
+            startActivity<TaskAdditionActivity>()
         }
-
+        
         val display = windowManager.defaultDisplay
         val point = Point()
         display.getSize(point)
-
+        
         globalValue.displayHeight = point.y
         globalValue.displayWidth = point.x
-
+        
     }
-
+    
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
         // Inflate the menu; this adds items to the action bar if it is present.
         menuInflater.inflate(R.menu.menu_main, menu)
         return true
     }
-
+    
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
         val id = item.itemId
-
-
+        
+        
         return if (id == R.id.action_settings) {
             true
         } else super.onOptionsItemSelected(item)
-
+        
     }
-
+    
     /**
      * A placeholder fragment containing a simple view.
      */
     class PlaceholderFragment : Fragment() {
-
+        
         override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                                   savedInstanceState: Bundle?): View? {
             val rootView = inflater.inflate(R.layout.fragment_main, container, false)
@@ -109,14 +99,14 @@ class MainActivity : AppCompatActivity() {
             textView.text = getString(R.string.section_format, arguments.getInt(ARG_SECTION_NUMBER))
             return rootView
         }
-
+        
         companion object {
             /**
              * The fragment argument representing the section number for this
              * fragment.
              */
             private val ARG_SECTION_NUMBER = "section_number"
-
+            
             /**
              * Returns a new instance of this fragment for the given section
              * number.
@@ -130,32 +120,26 @@ class MainActivity : AppCompatActivity() {
             }
         }
     }
-
+    
     /**
      * A [FragmentPagerAdapter] that returns a fragment corresponding to
      * one of the sections/tabs/pages.
      */
     private inner class SectionsPagerAdapter internal constructor(fm: FragmentManager) : FragmentPagerAdapter(fm) {
-
+        
         //TODO : Show screen what was chosen.
-        override fun getItem(position: Int): Fragment {
-            // getItem is called to instantiate the fragment for the given page.
-            // Return a PlaceholderFragment (defined as a static inner class below).
-            if (position == 0) {
-                return CalendarFragment()
-            } else if (position == 1) {
-                return ToDoListFragment()
-            } else if (position == 2) {
-                return TimerSetTimeFragment()
-            }
-            return PlaceholderFragment.newInstance(position + 1)
+        // Return a PlaceholderFragment (defined as a static inner class below).
+        // getItem is called to instantiate the fragment for the given page.
+        override fun getItem(position: Int): Fragment = when (position) {
+            0 -> CalendarFragment()
+            1 -> ToDoListFragment()
+            2 -> TimerSetTimeFragment()
+            else -> PlaceholderFragment.newInstance(position + 1)
         }
-
-        override fun getCount(): Int {
-            // Show 3 total pages.
-            return 3
-        }
-
+        
+        // Show 3 total pages.
+        override fun getCount(): Int = 3
+        
         override fun getPageTitle(position: Int): CharSequence? {
             when (position) {
                 0 -> return getString(R.string.main_tab1)
