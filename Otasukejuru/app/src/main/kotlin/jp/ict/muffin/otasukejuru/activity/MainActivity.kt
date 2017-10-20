a58f11815c5f051abf8aa885a428944295cea222
mitohato <mitohato14@gmail.com>
2017-10-20 21:39:56 +0900

    Clean up

--- Otasukejuru/app/src/main/kotlin/jp/ict/muffin/otasukejuru/activity/MainActivity.kt
+++ Otasukejuru/app/src/main/kotlin/jp/ict/muffin/otasukejuru/activity/MainActivity.kt
@@ -2,19 +2,17 @@ package jp.ict.muffin.otasukejuru.activity
 
 import android.graphics.Point
 import android.os.Bundle
-import android.support.design.widget.FloatingActionButton
 import android.support.v4.app.Fragment
 import android.support.v4.app.FragmentManager
 import android.support.v4.app.FragmentPagerAdapter
 import android.support.v7.app.AppCompatActivity
-import android.view.*
-import android.widget.TextView
+import android.view.Menu
+import android.view.MenuItem
 import jp.ict.muffin.otasukejuru.R
 import jp.ict.muffin.otasukejuru.`object`.GlobalValue
 import jp.ict.muffin.otasukejuru.fragment.CalendarFragment
 import jp.ict.muffin.otasukejuru.fragment.TimerSetTimeFragment
 import jp.ict.muffin.otasukejuru.fragment.ToDoListFragment
-import jp.ict.muffin.otasukejuru.view.HoldableViewPager
 import kotlinx.android.synthetic.main.activity_main.*
 import org.jetbrains.anko.startActivity
 import java.util.*
@@ -48,13 +46,13 @@ class MainActivity : AppCompatActivity() {
       The {@link ViewPager} that will host the section contents.
      */
         
-        val mViewPager = findViewById(R.id.view_pager) as HoldableViewPager
-        view_pager.adapter = mSectionsPagerAdapter
-        view_pager.setSwipeHoldVar(true)
+        view_pager.apply {
+            adapter = mSectionsPagerAdapter
+            setSwipeHoldVar(true)
+        }
         
-        tabs.setupWithViewPager(mViewPager)
+        tabs.setupWithViewPager(view_pager)
         
-        val fab = findViewById(R.id.fab) as FloatingActionButton
         fab.setOnClickListener {
             startActivity<TaskAdditionActivity>()
         }
@@ -63,8 +61,10 @@ class MainActivity : AppCompatActivity() {
         val point = Point()
         display.getSize(point)
         
-        globalValue.displayHeight = point.y
-        globalValue.displayWidth = point.x
+        globalValue.apply {
+            displayHeight = point.y
+            displayWidth = point.x
+        }
         
     }
     
@@ -74,52 +74,15 @@ class MainActivity : AppCompatActivity() {
         return true
     }
     
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        val id = item.itemId
-        
-        
-        return if (id == R.id.action_settings) {
-            true
-        } else super.onOptionsItemSelected(item)
-        
-    }
-    
-    /**
-     * A placeholder fragment containing a simple view.
-     */
-    class PlaceholderFragment : Fragment() {
-        
-        override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
-                                  savedInstanceState: Bundle?): View? {
-            val rootView = inflater.inflate(R.layout.fragment_main, container, false)
-            val textView = rootView.findViewById(R.id.section_label) as TextView
-            textView.text = getString(R.string.section_format, arguments.getInt(ARG_SECTION_NUMBER))
-            return rootView
-        }
-        
-        companion object {
-            /**
-             * The fragment argument representing the section number for this
-             * fragment.
-             */
-            private val ARG_SECTION_NUMBER = "section_number"
-            
-            /**
-             * Returns a new instance of this fragment for the given section
-             * number.
-             */
-            fun newInstance(sectionNumber: Int): PlaceholderFragment {
-                val fragment = PlaceholderFragment()
-                val args = Bundle()
-                args.putInt(ARG_SECTION_NUMBER, sectionNumber)
-                fragment.arguments = args
-                return fragment
+    // Handle action bar item clicks here. The action bar will
+    // automatically handle clicks on the Home/Up button, so long
+    // as you specify a parent activity in AndroidManifest.xml.
+    override fun onOptionsItemSelected(item: MenuItem): Boolean =
+            if (item.itemId == R.id.action_settings) {
+                true
+            } else {
+                super.onOptionsItemSelected(item)
             }
-        }
-    }
     
     /**
      * A [FragmentPagerAdapter] that returns a fragment corresponding to
@@ -131,23 +94,21 @@ class MainActivity : AppCompatActivity() {
         //TODO : Show screen what was chosen.
         // Return a PlaceholderFragment (defined as a static inner class below).
         // getItem is called to instantiate the fragment for the given page.
-        override fun getItem(position: Int): Fragment = when (position) {
+        override fun getItem(position: Int): Fragment? = when (position) {
             0 -> CalendarFragment()
             1 -> ToDoListFragment()
             2 -> TimerSetTimeFragment()
-            else -> PlaceholderFragment.newInstance(position + 1)
+            else -> null
         }
         
         // Show 3 total pages.
         override fun getCount(): Int = 3
         
-        override fun getPageTitle(position: Int): CharSequence? {
-            when (position) {
-                0 -> return getString(R.string.main_tab1)
-                1 -> return getString(R.string.main_tab2)
-                2 -> return getString(R.string.main_tab3)
-            }
-            return null
+        override fun getPageTitle(position: Int): CharSequence? = when (position) {
+            0 -> getString(R.string.main_tab1)
+            1 -> getString(R.string.main_tab2)
+            2 -> getString(R.string.main_tab3)
+            else -> null
         }
     }
 }
