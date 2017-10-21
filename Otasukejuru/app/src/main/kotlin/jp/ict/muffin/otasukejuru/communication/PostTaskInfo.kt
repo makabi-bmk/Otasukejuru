57a10f16663a1bde1605c0069251328504ee790c
mitohato <mitohato14@gmail.com>
2017-10-21 15:31:35 +0900

    Add PostTaskInfo

---
+++ Otasukejuru/app/src/main/kotlin/jp/ict/muffin/otasukejuru/communication/PostTaskInfo.kt
@@ -0,0 +1,19 @@
+package jp.ict.muffin.otasukejuru.communication
+
+import jp.ict.muffin.otasukejuru.`object`.TaskInfo
+
+
+class PostTaskInfo() {
+    
+    fun post(taskInfo: TaskInfo) {
+        
+        
+        
+    }
+    
+    fun convertToJson(taskInfo: TaskInfo) {
+        
+        
+        
+    }
+}
