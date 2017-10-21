39d21a5c762768d9c887eb7fb2032e1ea1158064
mitohato <mitohato14@gmail.com>
2017-10-21 22:24:59 +0900

    Add getInformation

---
+++ Otasukejuru/app/src/main/kotlin/jp/ict/muffin/otasukejuru/communication/getInformation.kt
@@ -0,0 +1,59 @@
+package jp.ict.muffin.otasukejuru.communication
+
+import com.squareup.moshi.Moshi
+import jp.ict.muffin.otasukejuru.`object`.GlobalValue
+import jp.ict.muffin.otasukejuru.`object`.ScheduleInfo
+import jp.ict.muffin.otasukejuru.`object`.TaskInfo
+import okhttp3.OkHttpClient
+import okhttp3.Request
+import okhttp3.Response
+import org.json.JSONArray
+import java.io.IOException
+
+
+class getInformation {
+    private val client = OkHttpClient()
+    
+    private fun run(url: String): String {
+        val request = Request.Builder()
+                .url(url)
+                .build()
+        
+        var response: Response? = null
+        try {
+            response = client.newCall(request).execute()
+        } catch (e: IOException) {
+            e.printStackTrace()
+        }
+        return response?.body().toString()
+    }
+    
+    fun getTaskInfo() {
+        val response = run("${GlobalValue.SERVER_URL}/get/todo_list")
+        val moshi = Moshi.Builder().build()
+        val adapter = moshi.adapter(TaskInfo::class.java)
+        
+        
+        val jsonArray = JSONArray(response)
+        (0 until jsonArray.length()).forEach { i ->
+            val taskJSON = jsonArray.getJSONObject(i).toString()
+            adapter.fromJson(taskJSON)?.let { GlobalValue.taskInfoArrayList.add(it) }
+            
+        }
+        
+    }
+    
+    fun getCalendarInfo() {
+        val response = run("${GlobalValue.SERVER_URL}/get/calendar")
+    
+        val moshi = Moshi.Builder().build()
+        val adapter = moshi.adapter(ScheduleInfo::class.java)
+    
+        val jsonArray = JSONArray(response)
+        (0 until jsonArray.length()).forEach { i ->
+            val taskJSON = jsonArray.getJSONObject(i).toString()
+            adapter.fromJson(taskJSON)?.let { GlobalValue.scheduleInfoArrayList.add(it) }
+        
+        }
+    }
+}
