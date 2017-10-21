6b0671f11565f28d724e14d6d1165e8fe85408ce
mitohato <mitohato14@gmail.com>
2017-10-21 17:49:05 +0900

    Add EveryInfo

---
+++ Otasukejuru/app/src/main/kotlin/jp/ict/muffin/otasukejuru/object/EveryInfo.kt
@@ -0,0 +1,10 @@
+package jp.ict.muffin.otasukejuru.`object`
+
+
+data class EveryInfo(
+        var every_name: String = "",
+        var start_date: String = "",
+        var end_date: String = "",
+        var notice: Int = 0,
+        var repeat_type: Int = 0
+)
