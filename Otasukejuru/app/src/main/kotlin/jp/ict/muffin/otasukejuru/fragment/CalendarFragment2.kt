7bb5e2f3b8ab07528fda1b6692d1b100ab745ebe
mitohato <mitohato14@gmail.com>
2017-10-22 13:23:48 +0900

    Add card calendar

--- Otasukejuru/app/src/main/kotlin/jp/ict/muffin/otasukejuru/fragment/CalendarFragment2.kt
+++ Otasukejuru/app/src/main/kotlin/jp/ict/muffin/otasukejuru/fragment/CalendarFragment2.kt
@@ -1,20 +1,27 @@
 package jp.ict.muffin.otasukejuru.fragment
 
+import android.content.Context.LAYOUT_INFLATER_SERVICE
 import android.os.Bundle
 import android.support.v4.app.Fragment
+import android.support.v4.content.ContextCompat
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.LinearLayout
+import jp.ict.muffin.otasukejuru.R
 import jp.ict.muffin.otasukejuru.`object`.GlobalValue
+import jp.ict.muffin.otasukejuru.`object`.TaskInfo
 import jp.ict.muffin.otasukejuru.ui.CalendarFragmentUI
+import kotlinx.android.synthetic.main.task_card_view.*
 import org.jetbrains.anko.AnkoContext
+import org.jetbrains.anko.find
 import org.jetbrains.anko.support.v4.ctx
+import org.jetbrains.anko.textColor
+import java.util.*
 
 
 class CalendarFragment2 : Fragment() {
-    companion object {
-        fun getInstance(): CalendarFragment2 = CalendarFragment2()
-    }
     
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View =
@@ -23,9 +30,49 @@ class CalendarFragment2 : Fragment() {
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
         
+        val calendar = Calendar.getInstance()
+        val today = (calendar.get(Calendar.MONTH) + 1) * 100 + calendar.get(Calendar.DAY_OF_MONTH)
         val showTaskNum = GlobalValue.displayWidth / 90
-        (0 until showTaskNum).forEach {
+        
+        val forNum = minOf(showTaskNum, GlobalValue.taskInfoArrayList.size)
+        
+        (0 until forNum).forEach {
+            val taskInfo: TaskInfo = GlobalValue.taskInfoArrayList[it]
+            val diffDays = diffDayNum(today, taskInfo.limitDate, calendar.get(Calendar.YEAR))
+            
+            val inflater: LayoutInflater = context.getSystemService(LAYOUT_INFLATER_SERVICE) as LayoutInflater
+            val linearLayout: LinearLayout = inflater.inflate(R.layout.task_card_view, null) as LinearLayout
+            
+            linearLayout.apply {
+                dateTextView.apply {
+                    text = diffDays.toString()
+                    if (taskInfo.priority == 0) {
+                        textColor = ContextCompat.getColor(context, R.color.mostPriority)
+                    }
+                }
+                cardView.apply {
+                    tag = taskInfo.limitDate
+                }
+                taskNameTextView.text = taskInfo.task_name
+                find<LinearLayout>(R.id.taskLinear).addView(linearLayout, 0)
+            }
             
         }
+    }
+    
+    private fun diffDayNum(beforeDate: Int, afterDate: Int, year: Int): Int {
+        
+        val totalDays = if (year % 4 == 0 && year % 100 != 0 || year % 400 == 0) {
+            intArrayOf(0, 31, 59, 90, 120, 151, 181, 212, 243, 273, 304, 334)
+        } else {
+            intArrayOf(0, 31, 60, 91, 121, 152, 182, 213, 244, 274, 305, 335)
+        }
+        
+        Log.d("$beforeDate ", "$afterDate")
+        val beforeDay: Int = beforeDate % 100
+        val beforeMonth: Int = beforeDate / 100
+        val afterDay: Int = afterDate % 100
+        val afterMonth: Int = afterDate / 100
+        return (totalDays[afterMonth] + afterDay - (totalDays[beforeMonth] + beforeDay))
     }
 }
