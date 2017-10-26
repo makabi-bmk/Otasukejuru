7a2d7e6cd29ae663b60a04a5f616400bbd50c0f4
mitohato <mitohato14@gmail.com>
2017-10-27 06:14:46 +0900

    Fix getDate

--- Otasukejuru/app/src/main/kotlin/jp/ict/muffin/otasukejuru/other/SpltDate.kt
+++ Otasukejuru/app/src/main/kotlin/jp/ict/muffin/otasukejuru/other/SpltDate.kt
@@ -1,24 +1,24 @@
 package jp.ict.muffin.otasukejuru.other
 
-import android.util.Log
-
 
 class SpltDate {
-    fun getDate(str: String): Int {
-        if (str == "") {
+    fun getDate(time: String): Int {
+        if (time == "") {
             return 0
         }
-        val a = str.split(" ")
-        val b = a[0].split("-")
+        val binaryTime = time.split(" ")
+        val date = binaryTime[0].split("-")
         
-        Log.d(a.toString(), b.toString())
-        return Integer.parseInt(b[1]) * 100 + Integer.parseInt(b[2])
+        return Integer.parseInt(date[1]) * 100 + Integer.parseInt(date[2])
     }
     
-    fun getTime(str: String): Int {
-        val a = str.split(" ")
-        val b = a[1].split(":")
+    fun getTime(time: String): Int {
+        if (time == "") {
+            return 0
+        }
+        val binaryTime = time.split(" ")
+        val date = binaryTime[1].split(":")
         
-        return Integer.parseInt(b[0]) * 100 + Integer.parseInt(b[1])
+        return Integer.parseInt(date[0]) * 100 + Integer.parseInt(date[1])
     }
 }
