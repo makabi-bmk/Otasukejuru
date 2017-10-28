7380a0304e9a80542f8c12c6354bdf7af19dc5b2
mitohato <mitohato14@gmail.com>
2017-10-28 20:21:53 +0900

    Add text watcher

--- Otasukejuru/app/src/main/kotlin/jp/ict/muffin/otasukejuru/ui/TimerSetTimeFragmentUI.kt
+++ Otasukejuru/app/src/main/kotlin/jp/ict/muffin/otasukejuru/ui/TimerSetTimeFragmentUI.kt
@@ -2,8 +2,6 @@ package jp.ict.muffin.otasukejuru.ui
 
 import android.content.Context
 import android.graphics.Color
-import android.os.Handler
-import android.os.Looper
 import android.support.v4.content.ContextCompat
 import android.text.InputType
 import android.view.View
@@ -11,7 +9,6 @@ import android.widget.EditText
 import android.widget.FrameLayout
 import android.widget.TextView
 import jp.ict.muffin.otasukejuru.R
-import jp.ict.muffin.otasukejuru.`object`.GlobalValue
 import jp.ict.muffin.otasukejuru.activity.TimerActivity
 import jp.ict.muffin.otasukejuru.fragment.TimerSetTimeFragment
 import jp.ict.muffin.otasukejuru.view.CircleGraphView
@@ -33,32 +30,6 @@ class TimerSetTimeFragmentUI : AnkoComponent<TimerSetTimeFragment> {
                 width = matchParent
             }
             
-            circle = frameLayout {
-                id = R.id.circleFrame
-            }.lparams {
-                height = GlobalValue.displayWidth - 30
-                width = GlobalValue.displayWidth - 30
-                topMargin = 30
-                leftMargin = 30
-            }
-            circleMini = frameLayout {
-            }.lparams {
-                translationZ = 2F
-                height = GlobalValue.displayWidth / 3
-                width = GlobalValue.displayWidth / 3
-                topMargin = GlobalValue.displayWidth / 3 + 15
-                leftMargin = GlobalValue.displayWidth / 3 + 15
-                remainingHourText = textView("") {
-                    textSize = 40F
-                }.lparams {
-                    translationZ = 2F
-                    width = wrapContent
-                    height = wrapContent
-                    topMargin = GlobalValue.displayWidth / 2 - 40
-                    centerHorizontally()
-                }
-            }
-            
             relativeLayout {
                 
                 editTime = editText {
@@ -70,10 +41,19 @@ class TimerSetTimeFragmentUI : AnkoComponent<TimerSetTimeFragment> {
                 }
                 
                 button("次へ") {
-                    textColor = ContextCompat.getColor(context, R.color.colorPrimary)
+                    id = R.id.nextButton
+//                    textColor = ContextCompat.getColor(context, R.color.colorPrimary)
                     backgroundColor = Color.argb(0, 0, 0, 0)
+                    isEnabled = false
+                    textColor = Color.argb(0, 0, 0, 0)
+                    textSize = 20f
                     onClick {
-                        startButtonClickListener(getContext())
+                        val time = editTime.text.toString().toLong()
+                        editTime.apply {
+                            text.clear()
+                            editTime.clearFocus()
+                        }
+                        startActivity<TimerActivity>("time" to time)
                     }
                 }.lparams {
                     below(editTime)
@@ -84,37 +64,11 @@ class TimerSetTimeFragmentUI : AnkoComponent<TimerSetTimeFragment> {
                 
                 
             }.lparams {
-                below(R.id.circleFrame)
                 width = matchParent
                 height = wrapContent
+                centerHorizontally()
+                centerVertically()
             }
-        }
-    }
-    
-    private fun startButtonClickListener(context: Context) {
-        if (editTime.text.toString() != "") {
-            val time = editTime.text.toString().toLong()
-            editTime.apply {
-                text.clear()
-                editTime.clearFocus()
-            }
-            
-            val params: ArrayList<HashMap<String, Int>> = java.util.ArrayList()
-            
-            val map = HashMap<String, Int>()
-            map.put("color", ContextCompat.getColor(context, R.color.back))
-            map.put("value", 60)
-            params.add(map)
-            
-            val circleGraphView = CircleGraphView(context,
-                    params, true)
-            circleMini.addView(circleGraphView)
-            circleGraphView.startAnimation()
-            
-            drawCircle(context, time)
-            Handler(Looper.getMainLooper()).postDelayed({
-                context.startActivity<TimerActivity>("time" to time)
-            }, 2 * 1000)
         }
     }
     
