d2401daaa15f3c520ac89848dbae629fc443e0e4
mitohato <mitohato14@gmail.com>
2017-10-29 07:12:30 +0900

    Add InputProgressBarActivity

---
+++ Otasukejuru/app/src/main/kotlin/jp/ict/muffin/otasukejuru/activity/InputProgressActivity.kt
@@ -0,0 +1,11 @@
+package jp.ict.muffin.otasukejuru.activity
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+class InputProgressActivity : AppCompatActivity() {
+    
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
---
+++ Otasukejuru/app/src/main/kotlin/jp/ict/muffin/otasukejuru/ui/InputProgressActivityUI.kt
@@ -0,0 +1,42 @@
+package jp.ict.muffin.otasukejuru.ui
+
+import android.graphics.Color
+import android.support.v4.content.ContextCompat
+import android.view.View
+import jp.ict.muffin.otasukejuru.R
+import jp.ict.muffin.otasukejuru.`object`.GlobalValue
+import jp.ict.muffin.otasukejuru.activity.InputProgressActivity
+import org.jetbrains.anko.*
+import org.jetbrains.anko.sdk25.coroutines.onClick
+
+
+class InputProgressActivityUI : AnkoComponent<InputProgressActivity> {
+    override fun createView(ui: AnkoContext<InputProgressActivity>): View = with(ui) {
+        relativeLayout {
+            val progressBar = progressBar {
+                id = R.id.inputProgressBar
+                progress = GlobalValue.taskInfoArrayList[0].progress
+            }.lparams {
+                width = wrapContent
+                height = wrapContent
+                centerHorizontally()
+                centerVertically()
+            }
+            
+            button("確定") {
+                id = R.id.nextButton
+                backgroundColor = Color.argb(0, 0, 0, 0)
+                textColor = ContextCompat.getColor(context, R.color.colorPrimary)
+                textSize = 20f
+                onClick {
+                    GlobalValue.taskInfoArrayList[0].progress = progressBar.progress
+                }
+            }.lparams {
+                margin = 30
+                alignParentBottom()
+                alignParentRight()
+            }
+        }
+        
+    }
+}
