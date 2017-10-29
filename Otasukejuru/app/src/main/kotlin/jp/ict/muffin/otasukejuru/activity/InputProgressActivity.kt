5773ccdb90ed1618055dfb68eab4a6f79ae0ddb8
mitohato <mitohato14@gmail.com>
2017-10-29 10:56:40 +0900

    Add progress

--- Otasukejuru/app/src/main/kotlin/jp/ict/muffin/otasukejuru/activity/InputProgressActivity.kt
+++ Otasukejuru/app/src/main/kotlin/jp/ict/muffin/otasukejuru/activity/InputProgressActivity.kt
@@ -2,8 +2,15 @@ package jp.ict.muffin.otasukejuru.activity
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.widget.Button
+import android.widget.SeekBar
+import android.widget.TextView
+import jp.ict.muffin.otasukejuru.R
 import jp.ict.muffin.otasukejuru.ui.InputProgressActivityUI
+import org.jetbrains.anko.find
+import org.jetbrains.anko.sdk25.coroutines.onClick
 import org.jetbrains.anko.setContentView
+
 
 class InputProgressActivity : AppCompatActivity() {
     
@@ -11,5 +18,29 @@ class InputProgressActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         val index = intent.getIntExtra("index", 0)
         InputProgressActivityUI(index).setContentView(this)
+    }
+    
+    override fun onResume() {
+        super.onResume()
+        val seekBar = find<SeekBar>(R.id.inputProgressSeek)
+        
+        seekBar.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener {
+            //ツマミがドラッグされると呼ばれる
+            override fun onProgressChanged(seekBar: SeekBar,
+                                           progress: Int, fromUser: Boolean) {
+                find<TextView>(R.id.progressTextView).text = seekBar.progress.toString()
+            }
+            
+            //ツマミがタッチされた時に呼ばれる
+            override fun onStartTrackingTouch(seekBar: SeekBar) {}
+            
+            //ツマミがリリースされた時に呼ばれる
+            override fun onStopTrackingTouch(seekBar: SeekBar) {}
+            
+        })
+        
+        find<Button>(R.id.finishButton).onClick {
+            finish()
+        }
     }
 }
--- Otasukejuru/app/src/main/kotlin/jp/ict/muffin/otasukejuru/ui/InputProgressActivityUI.kt
+++ Otasukejuru/app/src/main/kotlin/jp/ict/muffin/otasukejuru/ui/InputProgressActivityUI.kt
@@ -6,6 +6,7 @@ import android.os.AsyncTask
 import android.support.v4.content.ContextCompat
 import android.util.Log
 import android.view.View
+import android.widget.SeekBar
 import com.squareup.moshi.Moshi
 import jp.ict.muffin.otasukejuru.R
 import jp.ict.muffin.otasukejuru.`object`.GlobalValue
@@ -20,25 +21,41 @@ import org.jetbrains.anko.sdk25.coroutines.onClick
 
 
 class InputProgressActivityUI(private val index: Int) : AnkoComponent<InputProgressActivity> {
+    private lateinit var seekBar: SeekBar
     override fun createView(ui: AnkoContext<InputProgressActivity>): View = with(ui) {
         relativeLayout {
-            val progressBar = progressBar {
-                id = R.id.inputProgressBar
-                progress = GlobalValue.taskInfoArrayList[index].progress
+            relativeLayout {
+                seekBar = seekBar {
+                    id = R.id.inputProgressSeek
+                    progress = GlobalValue.taskInfoArrayList[index].progress
+                }.lparams {
+                    width = matchParent
+                    height = wrapContent
+                    margin = dip(100)
+                }
+                
+                textView(GlobalValue.taskInfoArrayList[index].progress.toString()) {
+                    id = R.id.progressTextView
+                    text = seekBar.progress.toString()
+                    textSize = 20f
+                }.lparams {
+                    below(seekBar)
+                    centerHorizontally()
+                }
             }.lparams {
                 width = wrapContent
                 height = wrapContent
-                centerHorizontally()
                 centerVertically()
+                centerHorizontally()
             }
             
             button("確定") {
-                id = R.id.nextButton
+                id = R.id.finishButton
                 backgroundColor = Color.argb(0, 0, 0, 0)
                 textColor = ContextCompat.getColor(context, R.color.colorPrimary)
                 textSize = 20f
                 onClick {
-                    GlobalValue.taskInfoArrayList[index].progress = progressBar.progress
+                    GlobalValue.taskInfoArrayList[index].progress = seekBar.progress
                     postProgress()
                 }
             }.lparams {
