3edf540b020766f27d45223312672e66e3744963
mitohato <mitohato14@gmail.com>
2017-10-31 06:56:19 +0900

    Create delete task async

--- Otasukejuru/app/src/main/kotlin/jp/ict/muffin/otasukejuru/fragment/TaskListFragment.kt
+++ Otasukejuru/app/src/main/kotlin/jp/ict/muffin/otasukejuru/fragment/TaskListFragment.kt
@@ -1,33 +1,28 @@
 package jp.ict.muffin.otasukejuru.fragment
 
-import android.annotation.SuppressLint
 import android.app.AlertDialog
 import android.content.Context.LAYOUT_INFLATER_SERVICE
-import android.os.AsyncTask
 import android.os.Bundle
 import android.os.Handler
 import android.support.v4.app.Fragment
 import android.support.v4.content.ContextCompat
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.LinearLayout
 import android.widget.RelativeLayout
-import com.squareup.moshi.Moshi
 import jp.ict.muffin.otasukejuru.R
 import jp.ict.muffin.otasukejuru.`object`.GlobalValue
 import jp.ict.muffin.otasukejuru.`object`.TaskInfo
 import jp.ict.muffin.otasukejuru.activity.InputProgressActivity
 import jp.ict.muffin.otasukejuru.activity.TaskAdditionActivity
+import jp.ict.muffin.otasukejuru.communication.DeleteTaskInfoAsync
 import jp.ict.muffin.otasukejuru.other.Utils
 import jp.ict.muffin.otasukejuru.ui.TaskListFragmentUI
 import kotlinx.android.synthetic.main.fragment_list_todo.*
 import kotlinx.android.synthetic.main.task_card_view.view.*
 import okhttp3.MediaType
 import okhttp3.OkHttpClient
-import okhttp3.Request
-import okhttp3.RequestBody
 import org.jetbrains.anko.AnkoContext
 import org.jetbrains.anko.collections.forEachWithIndex
 import org.jetbrains.anko.dip
@@ -226,42 +221,8 @@ class TaskListFragment : Fragment() {
     private val client = OkHttpClient()
     
     private fun deleteTask(element: TaskInfo, index: Int) {
-        val task = @SuppressLint("StaticFieldLeak")
-        object : AsyncTask<Unit, Unit, Unit>() {
-            override fun doInBackground(vararg params: Unit?) {
-                post("${GlobalValue.SERVER_URL}/delete/task",
-                        convertToJson(GlobalValue.taskInfoArrayList[index]))
-                GlobalValue.taskInfoArrayList.remove(element)
-            }
-            
-            
-        }
-        
-        task.execute()
+        val deleteTaskAsync = DeleteTaskInfoAsync()
+        deleteTaskAsync.execute(GlobalValue.taskInfoArrayList[index])
+        GlobalValue.taskInfoArrayList.remove(element)
     }
-    
-    private fun post(url: String, json: String): String? {
-        try {
-            val body = RequestBody.create(mediaType, json)
-            val request = Request.Builder()
-                    .url(url)
-                    .post(body)
-                    .build()
-            val response = client.newCall(request).execute()
-            return response.body()?.string()
-        } catch (e: Exception) {
-            e.printStackTrace()
-        }
-        return null
-    }
-    
-    private fun convertToJson(taskInfo: TaskInfo): String {
-        val moshi = Moshi.Builder().build()
-        val adapter = moshi.adapter(TaskInfo::class.java)
-        
-        Log.d("postTask", adapter.toJson(taskInfo))
-        
-        return adapter.toJson(taskInfo)
-    }
-    
 }
