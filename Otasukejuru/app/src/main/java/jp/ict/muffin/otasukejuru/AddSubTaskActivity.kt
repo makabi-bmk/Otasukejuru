d675f93046b8c46db265f4e84ec1c67e2eb183c0
mitohato <mitohato14@gmail.com>
2017-11-02 20:47:41 +0900

    Add activity for sub task

---
+++ Otasukejuru/app/src/main/java/jp/ict/muffin/otasukejuru/AddSubTaskActivity.kt
@@ -0,0 +1,11 @@
+package jp.ict.muffin.otasukejuru
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+class AddSubTaskActivity : AppCompatActivity() {
+    
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
