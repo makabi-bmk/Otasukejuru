1fe44cc8f82f76954c4b2381043302e3b48e799a
Ubuntu <ubuntu@ip-172-31-12-167.ap-northeast-1.compute.internal>
2017-11-02 16:05:28 +0900

    時間が経ちすぎて何したか覚えてない

--- serverapp/dbcon.py
+++ serverapp/dbcon.py
@@ -20,6 +20,7 @@ every_col = db['every']
 schedule_col = db['schedule']
 friend_col = db['friend']
 task_col = db['task']
+task_count = 0
 
 res_list = []
 
@@ -45,43 +46,45 @@ def add_task(task_name: str, due_date: dt.datetime, task_type: int,
         "guide_time": guide_time,
         "progress": progress,
         "priority": priority,
+        "friend": False
         # "run_time": 0
     }
-    print(post)
+    # print(post)
     task_col.insert_one(post)
 
 
 def add_schedule(schedule_name: str, start_date: dt.datetime,
-                 end_date: dt.datetime, notice: int) -> None:
+                 end_date: dt.datetime) -> None:
     res = check_days(start_date, end_date)
     # 複数スケジュールだった場合、分割
-    if res["result"]:
-        for i, day in enumerate(res["days"]):
-            # 連日スケジュールの初日だけ通知
-            if i == 0:
-                post = {
-                    "schedule_name": schedule_name,
-                    "start_time": day[0],
-                    "end_time": day[1],
-                    "notice": notice
-                }
-            else:
-                post = {
-                    "schedule_name": schedule_name,
-                    "start_time": day[0],
-                    "end_time": day[1],
-                }
-            print(post)
-            schedule_col.insert_one(post)
-    else:
-        post = {
-            "schedule_name": schedule_name,
-            "start_time": res["day"][0],
-            "end_time": res["day"][1],
-            "notice": notice
-        }
-        print(post)
-        schedule_col.insert_one(post)
+    # もう分割しなくていい
+    # if res["result"]:
+    #     for i, day in enumerate(res["days"]):
+    #         # 連日スケジュールの初日だけ通知
+    #         print("add_schedule: now day", i)
+    #         if i == 0:
+    #             post = {
+    #                 "schedule_name": schedule_name,
+    #                 "start_time": day[0],
+    #                 "end_time": day[1],
+    #                 "notice": notice
+    #             }
+    #         else:
+    #             post = {
+    #                 "schedule_name": schedule_name,
+    #                 "start_time": day[0],
+    #                 "end_time": day[1],
+    #             }
+    #         print(post)
+    #         schedule_col.insert_one(post)
+    # else:
+    post = {
+        "schedule_name": schedule_name,
+        "start_time": start_date,
+        "end_time": end_date,
+        # "notice": notice
+    }
+    schedule_col.insert_one(post)
 
 
 def make_zero_time(date: dt.datetime) -> dt.datetime:
@@ -99,6 +102,7 @@ def add_day(date: dt.datetime, dist: int) -> dt.datetime:
 
 
 def check_days(start_time: dt.datetime, end_time: dt.datetime):
+    # print("check_days: s_time.day {}, e_time.day {}".format(start_time.day, end_time.day))
     if start_time.day != end_time.day:
         res = {
             "result": True,
@@ -128,7 +132,7 @@ def add_every(name: str, start_time: dt.datetime, end_time: dt.datetime,
         "notice": notice,
         "repeat_type": repeat_type,
     }
-    print(post)
+    # print(post)
     every_col.insert_one(post)
 
 
@@ -148,14 +152,15 @@ def update_task(object_id: str, update_items: dict, friend_flag=False):
     object_id = ObjectId(object_id)
     if friend_flag:
         update_items['friend'] = True
-    task_col.update({"_id": object_id}, {'$set': {update_items}}, upsert=True)
+    print(update_items)
+    task_col.update({"_id": object_id}, {'$set': update_items}, upsert=True)
 
 
 def update_schedule(object_id: str, update_items: dict, friend_flag=False):
     object_id = ObjectId(object_id)
     if friend_flag:
         update_items['friend'] = True
-    schedule_col.update({"_id": object_id}, {"$set": {update_items}},
+    schedule_col.update({"_id": object_id}, {"$set": update_items},
                         upsert=True)
 
 
@@ -163,19 +168,49 @@ def update_every(object_id: str, update_items: dict, friend_flag=False):
     object_id = ObjectId(object_id)
     if friend_flag:
         update_items['friend'] = True
-    every_col.update({"_id": object_id}, {'$set': {update_items}}, upsert=True)
+    every_col.update({"_id": object_id}, {'$set': update_items}, upsert=True)
+
+
+def set_priority(n_banme):
+    global task_count
+    if task_count <= 10:
+        if n_banme <= 0:
+            return 0
+        elif n_banme <= 2:
+            return 1
+        elif n_banme <= 5:
+            return 2
+        else:
+            return 3
+    else:
+        if n_banme <= 2:
+            return 0
+        elif n_banme <= 7:
+            return 1
+        elif n_banme <= 17:
+            return 2
+        else:
+            return 3
 
 
 def get_todo_list():
+    global task_count
     todo_list = []
-    for task in task_col.find():
+    now = dt.datetime.now()
+    task_count = len(list(task_col.find()))
+    print(task_count)
+    for i, task in enumerate(sorted(task_col.find(), key=lambda x: -(100 - x["progress"]) / (x["due_date"] - now).total_seconds() / 3600 + 1)):
         task["_id"] = str(task["_id"])
         task["registration_date"] = str(task["registration_date"])
         task["due_date"] = str(task["due_date"])
         task["guide_time"] = str(task["guide_time"])
+
+        task["priority"] = set_priority(i)
+        print(i, task["priority"])
+
         todo_list.append(task)
     logger.debug("{} | get_todo_list: {}".format(dt.datetime.now(), todo_list))
-    return json.dumps({"todo_list": todo_list})
+    return {"todo_list": todo_list}
 
 
 def get_calendar():
@@ -184,23 +219,23 @@ def get_calendar():
     f = []
     t = []
     for i in schedule_col.find():
-        i["_id"] = ObjectId(i["_id"])
-        i["start_date"] = str(i["start_date"])
-        i["end_date"] = str(i["end_date"])
+        i["_id"] = str(i["_id"])
+        i["start_time"] = str(i["start_time"])
+        i["end_time"] = str(i["end_time"])
         s.append(i)
     for i in every_col.find():
-        i["_id"] = ObjectId(i["_id"])
-        i["start_date"] = str(i["start_date"])
-        i["end_date"] = str(i["end_date"])
+        i["_id"] = str(i["_id"])
+        i["start_time"] = str(i["start_time"])
+        i["end_time"] = str(i["end_time"])
         e.append(i)
     for i in task_col.find({"friend": True}):
-        i["_id"] = ObjectId(i["_id"])
+        i["_id"] = str(i["_id"])
         i["due_date"] = str(i["due_date"])
         i["guide_time"] = str(i["guide_time"])
         i["registration_date"] = str(i["registration_date"])
         f.append(i)
     for i in task_col.find({"friend": {"$exists": False}}):
-        i["_id"] = ObjectId(i["_id"])
+        i["_id"] = str(i["_id"])
         i["due_date"] = str(i["due_date"])
         i["guide_time"] = str(i["guide_time"])
         i["registration_date"] = str(i["registration_date"])
@@ -209,13 +244,12 @@ def get_calendar():
         "schedule": s,
         "every": e,
         "friend": f,
-        "task": [i.pop("_id") for i in task_col.find(
-            {"friend": {"$exists": False}}).sort(
-                {"priority": -1},
-                {"progress": 1},
-                {"due_date": 1}
-            )
-        ]
+        "task": sorted(t, key=lambda x: (-x["priority"], x["progress"]))
+                # {"priority": -1},
+                # {"progress": 1},
+                # {"due_date": 1}
+            # )
+        # ]
     }
     return json.dumps(calendar)
 
@@ -227,3 +261,4 @@ if __name__ == '__main__':
     a = dt.datetime.now()
     r = check_days(a, a + dt.timedelta(days=0))
     print(r)
+
