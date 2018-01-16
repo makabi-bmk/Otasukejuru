aa89abcd3dfff9e58aadd9e88e5f7b3c4631740a
mitohato <mitohato14@gmail.com>
2018-01-16 15:41:00 +0900

    Change way to connection from okhttp to fuel

--- Otasukejuru/app/src/main/java/jp/ict/muffin/otasukejuru/communication/AddScheduleTaskInfoAsync.kt
+++ Otasukejuru/app/src/main/java/jp/ict/muffin/otasukejuru/communication/AddScheduleTaskInfoAsync.kt
@@ -4,32 +4,12 @@ import android.os.AsyncTask
 import com.squareup.moshi.Moshi
 import jp.ict.muffin.otasukejuru.`object`.GlobalValue
 import jp.ict.muffin.otasukejuru.`object`.ScheduleInfo
-import okhttp3.OkHttpClient
-import okhttp3.Request
-import okhttp3.RequestBody
 
 
 class AddScheduleTaskInfoAsync : AsyncTask<ScheduleInfo, Void, Unit>() {
-    private val client = OkHttpClient()
     
     override fun doInBackground(vararg params: ScheduleInfo) {
-        post("${GlobalValue.SERVER_URL}/add/schedule", convertToJson(params[0]))
-    }
-    
-    private fun post(url: String, json: String): String? {
-        try {
-            val body = RequestBody.create(GlobalValue.mediaType, json)
-            val request = Request.Builder().apply {
-                url(url)
-                post(body)
-            }.build()
-            val response = client.newCall(request).execute()
-            return response.body()?.string()
-        } catch (e: Exception) {
-            e.printStackTrace()
-        }
-        
-        return null
+        AddInfo().post("${GlobalValue.SERVER_URL}/add/schedule", convertToJson(params[0]))
     }
     
     private fun convertToJson(scheduleInfo: ScheduleInfo): String {
--- Otasukejuru/app/src/main/java/jp/ict/muffin/otasukejuru/communication/AddTaskInfoAsync.kt
+++ Otasukejuru/app/src/main/java/jp/ict/muffin/otasukejuru/communication/AddTaskInfoAsync.kt
@@ -4,31 +4,12 @@ import android.os.AsyncTask
 import com.squareup.moshi.Moshi
 import jp.ict.muffin.otasukejuru.`object`.GlobalValue
 import jp.ict.muffin.otasukejuru.`object`.TaskInfo
-import okhttp3.OkHttpClient
-import okhttp3.Request
-import okhttp3.RequestBody
 
 
 class AddTaskInfoAsync : AsyncTask<TaskInfo, Void, Unit>() {
-    private val client = OkHttpClient()
     
     override fun doInBackground(vararg params: TaskInfo) {
-        post("${GlobalValue.SERVER_URL}/add/task", convertToJson(params[0]))
-    }
-    
-    private fun post(url: String, json: String): String? {
-        try {
-            val body = RequestBody.create(GlobalValue.mediaType, json)
-            val request = Request.Builder().apply {
-                url(url)
-                post(body)
-            }.build()
-            val response = client.newCall(request).execute()
-            return response.body()?.string()
-        } catch (e: Exception) {
-            e.printStackTrace()
-        }
-        return null
+        AddInfo().post("${GlobalValue.SERVER_URL}/add/task", convertToJson(params[0]))
     }
     
     private fun convertToJson(taskInfo: TaskInfo): String {
