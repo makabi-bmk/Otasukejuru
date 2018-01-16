af2528d0142fd2f6a86a5d228fff4e27b7b1f265
mitohato <mitohato14@gmail.com>
2018-01-16 15:52:39 +0900

    Change way to Update and Delete communication

--- Otasukejuru/app/src/main/java/jp/ict/muffin/otasukejuru/communication/UpdateEveryInfoAsync.kt
+++ Otasukejuru/app/src/main/java/jp/ict/muffin/otasukejuru/communication/UpdateEveryInfoAsync.kt
@@ -4,31 +4,12 @@ import android.os.AsyncTask
 import com.squareup.moshi.Moshi
 import jp.ict.muffin.otasukejuru.`object`.EveryInfo
 import jp.ict.muffin.otasukejuru.`object`.GlobalValue
-import okhttp3.OkHttpClient
-import okhttp3.Request
-import okhttp3.RequestBody
 
 
 class UpdateEveryInfoAsync : AsyncTask<EveryInfo, Void, Unit>() {
-    private val client = OkHttpClient()
     
     override fun doInBackground(vararg params: EveryInfo) {
-        post("${GlobalValue.SERVER_URL}/update/schedule", convertToJson(params[0]))
-    }
-    
-    private fun post(url: String, json: String): String? {
-        try {
-            val body = RequestBody.create(GlobalValue.mediaType, json)
-            val request = Request.Builder().apply {
-                url(url)
-                post(body)
-            }.build()
-            val response = client.newCall(request).execute()
-            return response.body()?.string()
-        } catch (e: Exception) {
-            e.printStackTrace()
-        }
-        return null
+        PostInfo().post("${GlobalValue.SERVER_URL}/update/schedule", convertToJson(params[0]))
     }
     
     private fun convertToJson(everyInfo: EveryInfo): String {
--- Otasukejuru/app/src/main/java/jp/ict/muffin/otasukejuru/communication/UpdateScheduleInfoAsync.kt
+++ Otasukejuru/app/src/main/java/jp/ict/muffin/otasukejuru/communication/UpdateScheduleInfoAsync.kt
@@ -4,31 +4,12 @@ import android.os.AsyncTask
 import com.squareup.moshi.Moshi
 import jp.ict.muffin.otasukejuru.`object`.GlobalValue
 import jp.ict.muffin.otasukejuru.`object`.ScheduleInfo
-import okhttp3.OkHttpClient
-import okhttp3.Request
-import okhttp3.RequestBody
 
 
 class UpdateScheduleInfoAsync : AsyncTask<ScheduleInfo, Void, Unit>() {
-    private val client = OkHttpClient()
     
     override fun doInBackground(vararg params: ScheduleInfo) {
-        post("${GlobalValue.SERVER_URL}/update/schedule", convertToJson(params[0]))
-    }
-    
-    private fun post(url: String, json: String): String? {
-        try {
-            val body = RequestBody.create(GlobalValue.mediaType, json)
-            val request = Request.Builder().apply {
-                url(url)
-                post(body)
-            }.build()
-            val response = client.newCall(request).execute()
-            return response.body()?.string()
-        } catch (e: Exception) {
-            e.printStackTrace()
-        }
-        return null
+        PostInfo().post("${GlobalValue.SERVER_URL}/update/schedule", convertToJson(params[0]))
     }
     
     private fun convertToJson(scheduleInfo: ScheduleInfo): String {
