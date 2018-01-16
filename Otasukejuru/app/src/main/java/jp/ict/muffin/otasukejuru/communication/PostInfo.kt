2f052ee774f73de85a7cc6aee9fe4846368be6b2
mitohato <mitohato14@gmail.com>
2018-01-16 22:07:36 +0900

    Fix try and catch

--- Otasukejuru/app/src/main/java/jp/ict/muffin/otasukejuru/communication/PostInfo.kt
+++ Otasukejuru/app/src/main/java/jp/ict/muffin/otasukejuru/communication/PostInfo.kt
@@ -1,13 +1,21 @@
 package jp.ict.muffin.otasukejuru.communication
 
+import android.util.Log
 import com.github.kittinunf.fuel.httpPost
+import com.github.kittinunf.result.Result
 
 
 open class PostInfo {
-    fun post(url: String, jsonString: String): String? = try {
-        url.httpPost().body(jsonString).response().second.data.toString()
-    } catch (e: Exception) {
-        e.printStackTrace()
-        null
+    fun post(url: String, jsonString: String) {
+        url.httpPost().body(jsonString).response { _, response, result ->
+            when (result) {
+                is Result.Success -> {
+                    Log.d("FuelResponse", response.data.toString())
+                }
+                is Result.Failure -> {
+                    Log.d("FuelResponse", "Failure")
+                }
+            }
+        }
     }
 }
