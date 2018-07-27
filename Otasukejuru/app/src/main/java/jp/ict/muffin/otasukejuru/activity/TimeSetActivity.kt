6a1370388452941ff6708c62ed8dfbdf2cf52ce8
mitohato <mitohato14@gmail.com>
2018-07-27 10:57:58 +0900

    Add code what display and hide next button when write time to editText

--- Otasukejuru/app/src/main/java/jp/ict/muffin/otasukejuru/activity/TimeSetActivity.kt
+++ Otasukejuru/app/src/main/java/jp/ict/muffin/otasukejuru/activity/TimeSetActivity.kt
@@ -3,15 +3,49 @@ package jp.ict.muffin.otasukejuru.activity
 import android.app.Activity
 import android.content.Context
 import android.content.Intent
+import android.graphics.Color
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.widget.Button
+import android.widget.EditText
 import android.widget.ImageButton
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 import jp.ict.muffin.otasukejuru.R
 import jp.ict.muffin.otasukejuru.ui.TimeSetActivityUI
 import org.jetbrains.anko.find
 import org.jetbrains.anko.setContentView
 
-class TimeSetActivity : Activity() {
-
+class TimeSetActivity : AppCompatActivity(), TextWatcher {
+    override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+    }
+    
+    override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+    }
+    
+    override fun afterTextChanged(p0: Editable?) {
+        findViewById<Button>(R.id.nextButton)?.let {
+            if (p0.toString() != "") {
+                it.setTextColor(ContextCompat.getColor(
+                        this,
+                        R.color.colorPrimary
+                ))
+                it.isEnabled = true
+            } else {
+                
+                it.setTextColor(Color.argb(
+                        0,
+                        0,
+                        0,
+                        0
+                ))
+                it.isEnabled = false
+                
+            }
+        }
+    }
+    
     companion object {
         fun start(
             context: Context?,
@@ -43,5 +77,7 @@ class TimeSetActivity : Activity() {
         find<ImageButton>(R.id.ankoBack).setOnClickListener {
             finish()
         }
+        
+        find<EditText>(R.id.setTimeEdit).addTextChangedListener(this)
     }
 }
