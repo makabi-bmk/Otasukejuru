7cf39d545d403ae713fe8352bfde2ee877a117ee
mitohato <mitohato14@gmail.com>
2018-12-31 12:13:51 +0900

    Add fragment for timer

---
+++ Otasukejuru/app/src/main/java/jp/ict/muffin/otasukejuru/ui/timer/interval/TimerIntervalFragment.kt
@@ -0,0 +1,10 @@
+package jp.ict.muffin.otasukejuru.ui.timer.interval
+
+import androidx.fragment.app.Fragment
+
+/**
+ * Created by mitohato14 on 2018-12-31.
+ */
+class TimerIntervalFragment : Fragment() {
+
+}
---
+++ Otasukejuru/app/src/main/java/jp/ict/muffin/otasukejuru/ui/timer/notification/TimerNotificationFragment.kt
@@ -0,0 +1,7 @@
+package jp.ict.muffin.otasukejuru.ui.timer.notification
+
+/**
+ * Created by mitohato14 on 2018-12-31.
+ */
+class TimerNotificationFragment {
+}
