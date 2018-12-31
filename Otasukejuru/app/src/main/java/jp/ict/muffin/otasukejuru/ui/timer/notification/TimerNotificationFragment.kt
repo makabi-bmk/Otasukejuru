616fc658cef980d54c4320f42638a4ba166f2a77
mitohato <mitohato14@gmail.com>
2018-12-31 12:48:08 +0900

    Extend fragment

--- Otasukejuru/app/src/main/java/jp/ict/muffin/otasukejuru/ui/timer/notification/TimerNotificationFragment.kt
+++ Otasukejuru/app/src/main/java/jp/ict/muffin/otasukejuru/ui/timer/notification/TimerNotificationFragment.kt
@@ -1,7 +1,9 @@
 package jp.ict.muffin.otasukejuru.ui.timer.notification
 
+import androidx.fragment.app.Fragment
+
 /**
  * Created by mitohato14 on 2018-12-31.
  */
-class TimerNotificationFragment {
+class TimerNotificationFragment : Fragment() {
 }
